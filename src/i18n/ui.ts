2bbd27de76d3e7975d72027a1c360b63d8c72791
Nicolas Leroy <nicolas.leroy@synolia.com>
2025-02-24 09:23:40 +0100

    home wip

---
+++ src/i18n/ui.ts
@@ -0,0 +1,16 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français'
+};
+
+export const defaultLang = 'fr';
+export const showDefaultLang = false;
+
+export const ui = {
+    fr: {
+        'home.greeting': 'Bonjour, je m\'appelle Nicolas'
+    },
+    en: {
+        'home.greeting': 'Hello, my name is Nicolas'
+    }
+};
